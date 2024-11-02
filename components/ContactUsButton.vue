52b701303bf99faeb02f125fa15edae624055248
Emerson602 <emerson.nic@hotmail.com>
2024-11-02 01:53:32 -0300

    created home section

--- components/ContactUsButton.vue
+++ components/ContactUsButton.vue
@@ -5,12 +5,12 @@
         aria-label="Fale conosco pelo WhatsApp"
         class="
             bg-gray-800
+            hover:bg-gray-600
             text-white
             rounded
             py-2
             px-6
-            text-xl
-            animate-bounce
+            text-xl         
         ">
         Fale conosco
     </a>
--- components/Intro.vue
+++ components/Intro.vue
@@ -1,11 +1,25 @@
 <template>
-    <section>
-        <img src="https://picsum.photos/500/500" alt="banner">
-        <div>
-            <h1>Serviços de montagem e manutenção</h1>
-            <h2>Elevadores, escadas rolantes, refrigeração comercial, geradores e automação industrial</h2>
-            <ContactUsButton />
-            <p></p>
+    <section class="lg:flex lg:flex-row h-auto border-b-8 border-gray-800">        
+        <img class="lg:hidden w-full" src="https://picsum.photos/500/500" alt="banner">
+        <div class="
+            flex justify-center
+            items-center
+            flex-col
+            h-auto
+            w-full
+            lg:w-1/2
+            text-xl
+            uppercase 
+            text-balance             
+            text-center
+            py-20
+            p-2
+            bg-gradient-to-t from-gray-500 to-stone-100
+            ">
+            <h1 class="font-bold">Serviços de montagem e manutenção</h1>
+            <h2 class="my-10">Elevadores, escadas rolantes, refrigeração comercial, geradores e automação industrial</h2>
+            <ContactUsButton />        
         </div>
+        <img class="hidden lg:block lg:w-1/2" src="https://picsum.photos/500/500" alt="banner">
     </section>
 </template>

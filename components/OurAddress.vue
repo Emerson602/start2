eb3a8ba8dce5d0da1267ef14380c0247c673c9d0
Emerson602 <emerson.nic@hotmail.com>
2024-11-03 12:22:55 -0300

    updated location

--- components/OurAddress.vue
+++ components/OurAddress.vue
@@ -5,17 +5,10 @@
             <p class="mt-5 text-balance lg:w-3/4">Rua do comercio, 436, edf. Lobão Barreto - sala 507, Centro, Maceió - AL</p>                            
         </div>
         <div class="mt-5">
-            <p  class="mb-5 font-bold text-2xl lg:text-lg">Localização:</p>
-            <a :href="linkCoordinates" target="_blank">
-                <img class="w-10/12 sm:w-6/12 h-auto lg:w-64 md:h-64 m-auto lg:m-0" alt="localizaçao" src="~/assets/imgs/location-preview.webp">
-            </a>    
+            <p  class="mb-5 font-bold text-2xl lg:text-lg">Localização:</p>   
+            <iframe class="w-10/12 h-64 sm:w-6/12 lg:w-96 lg:h-80 m-auto lg:m-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17080.98888542704!2d-35.749035851343365!3d-9.662488478879148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x701457fa477416d%3A0x4928ded8759c6b42!2sCondom%C3%ADnio%20Lob%C3%A3o%20Barreto!5e1!3m2!1spt-BR!2sbr!4v1730646351003!5m2!1spt-BR!2sbr" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
-
-const linkCoordinates:string = 'https://www.google.com/maps/place/Condom%C3%ADnio+Lob%C3%A3o+Barreto/@-9.6648506,-35.752564,5556m/data=!3m1!1e3!4m6!3m5!1s0x701457fa477416d:0x4928ded8759c6b42!8m2!3d-9.6627764!4d-35.7377317!16s%2Fg%2F1tdlhh2r?entry=ttu&g_ep=EgoyMDI0MTAyNy4wIKXMDSoASAFQAw%3D%3D'
-
-</script>
 

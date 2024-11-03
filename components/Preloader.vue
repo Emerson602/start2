1c4fdd258ddd9fcd6f66df8f192c702ad65c6e7d
Emerson602 <emerson.nic@hotmail.com>
2024-11-03 11:36:37 -0300

    adjustments

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="isLoading" class="flex flex-col fixed inset-0 flex items-center justify-center bg-gray-800 z-50">     
       <Logo />        
-      <div class="flex">        
+      <div class="flex mt-5">        
         <div class="w-6 h-6 mr-2 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
         <span class="text-white text-1xl uppercase">Carregando...</span>
       </div>    

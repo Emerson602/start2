791e7c18c9e87bd2f0e043bd63d4c6fd6d217a5e
Emerson602 <emerson.nic@hotmail.com>
2024-11-07 16:03:59 -0300

    update icons

--- components/IndexPage/CompanyValues.vue
+++ components/IndexPage/CompanyValues.vue
@@ -5,19 +5,19 @@
             <div class="flex justify-center items-start flex-col text-xl">
                 <h1 class="mx-auto mx-0 uppercase">Na Start2 engenharia:</h1>
                 <div class="flex flex-row justify-center items-center mt-10">
-                    <img src="~/assets/imgs/icon-1.webp" alt="Serviço de qualidade icone">
+                    <img class="w-12 h-12" src="~/assets/imgs/icon-1.webp" alt="Serviço de qualidade icone">
                     <span class="ml-2">Serviço de qualidade</span>
                 </div>
                 <div class="flex flex-row justify-center items-center mt-5">
-                    <img src="~/assets/imgs/icon-2.webp" alt="Custo-benefício icone">
+                    <img class="w-12 h-12" src="~/assets/imgs/icon-2.webp" alt="Custo-benefício icone">
                     <span class="ml-2">Custo-benefício</span>
                 </div>
                 <div class="flex flex-row justify-center items-center mt-5">
-                    <img src="~/assets/imgs/icon-3.webp" alt="Profissionais qualificados icone">
+                    <img class="w-12 h-12" src="~/assets/imgs/icon-3.webp" alt="Profissionais qualificados icone">
                     <span class="ml-2">Profissionais qualificados</span>
                 </div>
                 <div class="flex flex-row justify-center items-center mt-5">
-                    <img src="~/assets/imgs/icon-4.webp" alt="Você pode confiar! icone">
+                    <img class="w-12 h-12" src="~/assets/imgs/icon-4.webp" alt="Você pode confiar! icone">
                     <span class="ml-2">Você pode confiar!</span>
                 </div>                 
             </div>

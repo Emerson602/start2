c419b6692529c6577e9bf6344b08da19730a100d
Emerson602 <emerson.nic@hotmail.com>
2024-11-06 21:07:30 -0300

    adjustment in the LinkToAffariDev component

--- components/LinkToAffaridev.vue
+++ components/LinkToAffaridev.vue
@@ -1,13 +1,53 @@
 <template>    
-    <div class="mt-20 lg:mt-10 text-base text-balance text-center lg:text-start">
-        <img class="w-14 h-14 mx-auto lg:mx-0" src="~/assets/imgs/affari-logo.webp" alt="affari logo">
-        <p>Site desenvolvido por:</p>
-        <p class="mb-8 font-bold uppercase">Affari Dev</p>        
-        <a href="affaridev.com.br" target="_blank" rel="noopener noreferrer">
-            <span class="bg-lime-800 hover:bg-lime-900 text-white py-3 px-6 rounded">Acesse o nosso site</span>
-        </a>
+    <div class="mt-20 lg:mt-10 text-2xl lg:text-base text-balance text-center lg:text-start">     
+        
+
+        <p class="font-bold text-2xl lg:text-lg">Site desenvolvido por:</p> 
+
+        <div id="affari-content" class="flex flex-col justify-center items-center mx-auto mt-5 h-auto pt-5 pb-7 bg-zinc-800 w-72 text-base">
+
+            <img class="w-14 h-14 mx-auto lg:mx-0" src="~/assets/imgs/affari-logo.webp" alt="affari logo">
+            
+            <p class="font-bold uppercase">
+                <span class="text-lime-800 py-1 pr-2 pl-4">Affari</span>
+                <span class="text-white py-1 pr-4">Development</span>
+            </p>
+            <a 
+                class="
+                    text-white
+                    uppercase
+                    mt-5
+                    py-3
+                    px-6
+                    bg-lime-800
+                    hover:bg-lime-900
+                    rounded-full
+                    " 
+                href="http://www.affaridev.com.br"
+                target="_blank"
+                rel="noopener noreferrer
+            ">
+                <span>Acesse o nosso site</span>
+            </a>
+
+        </div>    
+
+
         <hr class="block lg:hidden w-1/5 mt-20 bg-gray-500 h-0.5 mx-auto"> 
+
     </div>    
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Great+Vibes&family=Lobster&family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Satisfy&display=swap');
+
+#affari-content {
+  font-family: "Nunito", sans-serif;
+  font-optical-sizing: auto;  
+  font-style: normal;
+}
+
+</style>
+
 
 

ec50cb8d3249c0dc7922389472ff3341a3f74c1b
Emerson602 <emerson.nic@hotmail.com>
2024-11-10 19:23:54 -0300

    adjustment AboutUs component

--- components/AboutPage/AboutUs.vue
+++ components/AboutPage/AboutUs.vue
@@ -1,13 +1,13 @@
 <template>
     <section>
         <h1 class="text-3xl font-bold uppercase text-center">Sobre nós</h1>
-            <div class="mt-20 mx-auto w-4/5">
+            <div class="mt-20 mx-auto">
 
                 <div>
                     <h2 class="text-2xl font-bold uppercase text-center">Missão:</h2>
-                    <div class="flex flex-col lg:flex-row justify-center items-center">   
+                    <div class="flex flex-col justify-center items-center">   
                         <img class="my-10 w-48 h-48" src="~/assets/imgs/image-mission.webp" alt="Imagem representando a missão">
-                        <p class="lg:w-2/4 ml-10">
+                        <p class="w-4/5 lg:w-2/4 text-2xl text-start sm:text-center">
                             Desenvolver rotinas de trabalho que nos faça ir sempre além do esperado,
                             dando o melhor de cada envolvido na ação.
                         </p>
@@ -16,10 +16,10 @@
 
                 <div class="mt-20">
                     <h2 class="text-2xl font-bold uppercase text-center">Visão:</h2>
-                    <div class="flex flex-col lg:flex-row justify-center lg: items-center">
+                    <div class="flex flex-col justify-center lg: items-center">
                         <img class="my-10 w-48 h-48" src="~/assets/imgs/image-vision.webp" alt="Imagem representando a visão">
-                        <p class="lg:w-2/4 ml-10">
-                            Que nós como Empresa estejamos sempre buscando alcançar o horizonte,
+                        <p class="w-4/5 lg:w-2/4 text-2xl text-start sm:text-center">
+                            Que nós como empresa estejamos sempre buscando alcançar o horizonte,
                             com isso, não nos deixando cair na mediocridade.
                         </p>
                     </div>

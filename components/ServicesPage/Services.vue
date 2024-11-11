a0a7c219179ec003f8ba7b886c1b9e65f685cd9e
Emerson602 <emerson.nic@hotmail.com>
2024-11-11 00:27:05 -0300

    update Services component

--- components/ServicesPage/Services.vue
+++ components/ServicesPage/Services.vue
@@ -1,30 +1,37 @@
 <template>
 
-  <section>
-    <h1>Serviços</h1>
+  <section class="py-28 lg:py-32 text-balance text-center bg-gradient-to-t from-slate-300 to-stone-50 border-b-8 border-gray-800">
+
+    <h1 class="text-3xl font-bold uppercase text-center">Serviços</h1>
     
-    <div>
-      <img src="~/assets/imgs/image-1.webp">
-      <p>Descriçõ</p>
-      <img src="~/assets/imgs/image-1.webp">
+    <div class="mt-20 flex flex-col lg:flex-row justify-center items-center text-2xl">
+      <img class="w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-2.webp" alt="Serviços de montagem e manutenção de elevadores, esteiras e escadas rolantes.">
+      <p class="mt-10 lg:mx-10 lg:px-10 lg:mt-0 w-4/5 lg:w-96">
+        Serviços de montagem e manutenção de elevadores, esteiras e escadas rolantes.
+      </p>     
     </div>
 
-    <div>
-      <img src="~/assets/imgs/image-1.webp">
-      <p>Descriçõ</p>
-      <img src="~/assets/imgs/image-1.webp">
+    <div class="mt-20 flex flex-col lg:flex-row justify-center items-center text-2xl">
+      <img class="block lg:hidden w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-6.webp" alt="Serviços de montagem e manutenção de refrigeração e climatização comercial.">
+      <p class="mt-10 lg:mx-10 lg:px-10 lg:mt-0 w-4/5 lg:w-96">
+        Serviços de montagem e manutenção de refrigeração e climatização comercial.
+      </p>
+      <img class="hidden lg:block w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-6.webp" alt="Serviços de montagem e manutenção de refrigeração e climatização comercial.">
     </div>
 
-    <div>
-      <img src="~/assets/imgs/image-1.webp">
-      <p>Descriçõ</p>
-      <img src="~/assets/imgs/image-1.webp">
+    <div class="mt-20 flex flex-col lg:flex-row justify-center items-center text-2xl">
+      <img class="w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-1.webp" alt="Serviços de manutenção em geradores.">
+      <p class="mt-10 lg:mx-10 lg:px-10 lg:mt-0 w-4/5 lg:w-96">
+        Serviços de manutenção em geradores.
+      </p>     
     </div>
 
-    <div>
-      <img src="~/assets/imgs/image-1.webp">
-      <p>Descriçõ</p>
-      <img src="~/assets/imgs/image-1.webp">
+    <div class="mt-20 flex flex-col lg:flex-row justify-center items-center text-2xl">
+      <img class="block lg:hidden w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-4.webp" alt="Serviços de manutenção elétrica e automação industrial.">
+      <p class="mt-10 lg:mx-10 lg:px-10 lg:mt-0 w-4/5 lg:w-96">
+        Serviços de manutenção elétrica e automação industrial.
+      </p>
+      <img class="hidden lg:block w-4/5 lg:w-96 h-auto lg:h-96" src="~/assets/imgs/image-4.webp" alt="Serviços de manutenção elétrica e automação industrial.">
     </div>
 
   </section>

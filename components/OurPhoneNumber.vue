5753978e4a38e9292cf6a78eb57cf99a5b82d1d2
Emerson602 <emerson.nic@hotmail.com>
2024-11-11 14:19:24 -0300

    update phone number

--- components/OurPhoneNumber.vue
+++ components/OurPhoneNumber.vue
@@ -3,7 +3,7 @@
         <p class="my-5 font-bold text-2xl lg:text-lg">Telefone | Whatsapp:</p>
         <a class="text-blue-700 flex items-center justify-center lg:justify-start" :href="useGlobalData()" target="_blank" rel="noopener noreferrer">
             <img class="w-8 h-8 lg:w-6 lg:h-auto mr-1" alt="whatsapp icon" src="~/assets/imgs/whatsapp-icon.webp">
-            <span>(82) 99876-5432</span>
+            <span>(82) 99902-5007</span>
         </a>
     </div>
 </template>
